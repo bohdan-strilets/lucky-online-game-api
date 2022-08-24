80b096cb8726dbed96a3263540e2564ba348f0fe
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-24 19:27:27 +0200

    added statistics schema and model

---
+++ models/statistics.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const statisticsSchema = Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+    timeInGame: {
+      type: Number,
+      default: 0,
+    },
+    totalBets: {
+      type: Number,
+      default: 0,
+    },
+    wonBets: {
+      type: Number,
+      default: 0,
+    },
+    lostBets: {
+      type: Number,
+      default: 0,
+    },
+    moneySpent: {
+      type: Number,
+      default: 0,
+    },
+    moneyEarned: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Statistics = model("statistics", statisticsSchema);
+
+module.exports = {
+  Statistics,
+};
