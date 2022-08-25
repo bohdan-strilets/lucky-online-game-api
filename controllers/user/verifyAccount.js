956020e85d961bc43b1f9d2aeecdc2e6d63fefe5
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-25 18:15:37 +0200

    added verify account controller

--- controllers/user/verifyAccount.js
+++ controllers/user/verifyAccount.js
@@ -1,3 +1,27 @@
-const verifyAccount = async (req, res) => {};
+const { User } = require("../../models");
+
+const verifyAccount = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+
+  if (!user) {
+    return res.status(404).json({
+      status: "error",
+      code: 404,
+      message: "User not found.",
+    });
+  }
+
+  await User.findByIdAndUpdate(user._id, {
+    isVerified: true,
+    verificationToken: null,
+  });
+
+  res.json({
+    status: "ok",
+    code: 200,
+    message: "Verification successful.",
+  });
+};
 
 module.exports = verifyAccount;
