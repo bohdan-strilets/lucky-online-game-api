0f81e6e82a085d472bea80b6021f1d6592bfbdf2
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-25 18:13:29 +0200

    added change avatar controller

--- controllers/user/changeAvatar.js
+++ controllers/user/changeAvatar.js
@@ -1,3 +1,37 @@
-const changeAvatar = async (req, res) => {};
+const { User } = require("../../models");
+const path = require("path");
+const fs = require("fs/promises");
+const jimp = require("jimp");
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const changeAvatar = async (req, res) => {
+  const { path: tmpUpload, originalname } = req.file;
+  const { _id } = req.user;
+  const imageName = `${_id}_${originalname}`;
+
+  try {
+    const resultUpload = path.join(avatarsDir, imageName);
+
+    jimp
+      .read(tmpUpload)
+      .then((image) => image.resize(250, 250).write(resultUpload))
+      .catch((error) => console.log(error));
+
+    fs.unlink(tmpUpload);
+
+    const avatarURL = path.join("avatars", imageName);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL });
+
+    return res.json({
+      status: "ok",
+      code: 200,
+      avatarURL,
+    });
+  } catch (error) {
+    fs.unlink(tmpUpload);
+    throw error;
+  }
+};
 
 module.exports = changeAvatar;
