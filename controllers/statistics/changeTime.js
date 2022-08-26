9193dfa4005cd6812a338269bde4ff9a35e05486
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-26 19:32:27 +0200

    added change time controoler

--- controllers/statistics/changeTime.js
+++ controllers/statistics/changeTime.js
@@ -1,3 +1,24 @@
-const changeTime = async (req, res) => {};
+const { Statistics } = require("../../models");
+
+const changeTime = async (req, res) => {
+  const { _id } = req.user;
+  const { timeInGame } = req.body;
+
+  const statistics = await Statistics.findOne({ owner: _id });
+
+  const result = await Statistics.findByIdAndUpdate(
+    statistics._id,
+    {
+      $inc: { timeInGame },
+    },
+    { new: true }
+  );
+
+  return res.json({
+    status: "ok",
+    code: 200,
+    timeInGame: result.timeInGame,
+  });
+};
 
 module.exports = changeTime;
