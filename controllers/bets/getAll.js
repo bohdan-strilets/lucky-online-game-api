b06dbb1d0f7830295c0e67f3f7801973d51b5559
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-26 04:53:45 +0200

    added get all bet controller

--- controllers/bets/getAll.js
+++ controllers/bets/getAll.js
@@ -1,5 +1,22 @@
-const { Bets } = require("../../models");
+const { Bet } = require("../../models");
 
-const getAll = async (req, res) => {};
+const getAll = async (req, res) => {
+  const { _id } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const bets = await Bet.find({ owner: _id }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "_id name avatarURL");
+
+  return res.json({
+    status: "ok",
+    code: 200,
+    bets,
+    page,
+    limit,
+  });
+};
 
 module.exports = getAll;
