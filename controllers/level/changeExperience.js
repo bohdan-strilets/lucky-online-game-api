1096243a57448c8b19a81384e1645b75a4a23e73
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-26 20:04:35 +0200

    added change experience controoler

--- controllers/level/changeExperience.js
+++ controllers/level/changeExperience.js
@@ -1,3 +1,24 @@
-const changeExperience = async (req, res) => {};
+const { Level } = require("../../models");
+
+const changeExperience = async (req, res) => {
+  const { _id } = req.user;
+  const { experience } = req.body;
+
+  const levelInfo = await Level.findOne({ owner: _id });
+
+  const result = await Level.findByIdAndUpdate(
+    levelInfo._id,
+    {
+      $inc: { experience },
+    },
+    { new: true }
+  );
+
+  return res.json({
+    status: "ok",
+    code: 200,
+    level: result.experience,
+  });
+};
 
 module.exports = changeExperience;
