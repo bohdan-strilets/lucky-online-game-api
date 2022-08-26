fcc1cc3cfde7bf6a8a62c6c14294a9528801d5f3
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-26 20:18:34 +0200

    update create bet controller

--- controllers/bets/createBet.js
+++ controllers/bets/createBet.js
@@ -1,8 +1,20 @@
-const { Bet } = require("../../models");
+const { Bet, Statistics } = require("../../models");
 
 const createBet = async (req, res) => {
   const { _id } = req.user;
-  const result = await Bet.create({ owner: _id, ...req.body });
+  const bet = req.body;
+
+  const result = await Bet.create({ owner: _id, ...bet });
+  const statistics = await Statistics.findOne({ owner: _id });
+  const lostBets = statistics.totalBets - statistics.wonBets;
+
+  await Statistics.findByIdAndUpdate(statistics._id, {
+    $inc: {
+      totalBets: 1,
+      moneySpent: bet.betAmount,
+      lostBets,
+    },
+  });
 
   return res.status(201).json({
     status: "ok",
