5379c362bb946591db406d0f513725a5709bc773
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-26 20:02:38 +0200

    added change rank controoler

--- controllers/level/changeRank.js
+++ controllers/level/changeRank.js
@@ -1,3 +1,22 @@
-const changeRank = async (req, res) => {};
+const { Level } = require("../../models");
+
+const changeRank = async (req, res) => {
+  const { _id } = req.user;
+  const { rank } = req.body;
+
+  const levelInfo = await Level.findOne({ owner: _id });
+
+  const result = await Level.findByIdAndUpdate(
+    levelInfo._id,
+    { rank },
+    { new: true }
+  );
+
+  return res.json({
+    status: "ok",
+    code: 200,
+    level: result.rank,
+  });
+};
 
 module.exports = changeRank;
