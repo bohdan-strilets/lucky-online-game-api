9cdd91e760b491ca23f0a8d3fc4fe93310a9a822
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-26 04:57:06 +0200

    added get one bet controller

--- controllers/bets/getOne.js
+++ controllers/bets/getOne.js
@@ -1,3 +1,25 @@
-const getOne = async (req, res) => {};
+const { Bet } = require("../../models");
+
+const getOne = async (req, res) => {
+  const { betId } = req.params;
+  const result = await Bet.findById(betId).populate(
+    "owner",
+    "_id name avatarURL"
+  );
+
+  if (!result) {
+    return res.status(404).json({
+      status: "error",
+      code: 404,
+      message: "Not found.",
+    });
+  }
+
+  return res.json({
+    status: "ok",
+    code: 200,
+    bet: result,
+  });
+};
 
 module.exports = getOne;
