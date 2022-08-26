dc281051d06043520de811ca92c3e7d7a341d271
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-26 19:59:54 +0200

    added change level controoler

--- controllers/level/changeLevel.js
+++ controllers/level/changeLevel.js
@@ -1,3 +1,24 @@
-const changeLevel = async (req, res) => {};
+const { Level } = require("../../models");
+
+const changeLevel = async (req, res) => {
+  const { _id } = req.user;
+  const { level } = req.body;
+
+  const levelInfo = await Level.findOne({ owner: _id });
+
+  const result = await Level.findByIdAndUpdate(
+    levelInfo._id,
+    {
+      $inc: { level },
+    },
+    { new: true }
+  );
+
+  return res.json({
+    status: "ok",
+    code: 200,
+    level: result.level,
+  });
+};
 
 module.exports = changeLevel;
